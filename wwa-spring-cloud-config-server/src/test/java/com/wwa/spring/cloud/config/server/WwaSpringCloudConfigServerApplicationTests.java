b21871fda725889b6ac4890fd62c58511b30b9e6
Ram Babu Verma <ram.verma@yash.com>
2024-01-19 23:39:33 +0530

    Added spring cloud config server setup application

---
+++ wwa-spring-cloud-config-server/src/test/java/com/wwa/spring/cloud/config/server/WwaSpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wwa.spring.cloud.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwaSpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
